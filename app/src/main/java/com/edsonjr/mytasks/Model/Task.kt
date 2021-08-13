c6a93cf21bc7e7b6bfc6aa32c5adf5d6f592acee
eddsonjr <eddson.jr@gmail.com>
2021-08-12 21:56:52 -0400

    Implementado os annotations do ROOM na classe de model Tasks
    Signed-off-by: eddsonjr <eddson.jr@gmail.com>

--- app/src/main/java/com/edsonjr/mytasks/Model/Task.kt
+++ app/src/main/java/com/edsonjr/mytasks/Model/Task.kt
@@ -1,10 +1,33 @@
 package com.edsonjr.mytasks.Model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "task_table")
 data class Task(
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int,
+
+    @ColumnInfo(name = "title")
     val title: String,
+
+    @ColumnInfo(name = "description")
     val description: String?,
+
+    @ColumnInfo(name = "date")
     val date: String?,
+
+
+    @ColumnInfo(name = "hour")
     val hour: String?,
+
+    @ColumnInfo(name = "important")
     val important: Boolean = false,
+
+    @ColumnInfo(name = "completed")
     val completed: Boolean = false
 )
