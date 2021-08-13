e48c9a63793e110e082dce43e778b6273a20b84f
eddsonjr <eddson.jr@gmail.com>
2021-08-12 22:38:05 -0400

    [TaskDAO] - Implementada a interface de DAO para as Tasks
    Signed-off-by: eddsonjr <eddson.jr@gmail.com>

---
+++ app/src/main/java/com/edsonjr/mytasks/DataBase/TaskDAO.kt
@@ -0,0 +1,29 @@
+package com.edsonjr.mytasks.DataBase
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.edsonjr.mytasks.Model.Task
+
+
+//interface para trabalhar com o DAO
+@Dao
+interface TaskDAO {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task): Long
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Query("DELETE FROM task_table")
+    suspend fun deleteAll()
+    
+
+    @Query("SELECT * FROM task_table")
+    fun getAllTasks(): LiveData<List<Task>>
+
+
+}
