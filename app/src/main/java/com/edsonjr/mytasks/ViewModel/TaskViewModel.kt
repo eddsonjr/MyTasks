2163cba5d1f75edf0112924406516606268b5645
eddsonjr <eddson.jr@gmail.com>
2021-08-14 18:15:29 -0400

    [TaskViewModel] - Implementado as principais funcoes de crud no ViewModl de tasks
    Signed-off-by: eddsonjr <eddson.jr@gmail.com>

--- app/src/main/java/com/edsonjr/mytasks/ViewModel/TaskViewModel.kt
+++ app/src/main/java/com/edsonjr/mytasks/ViewModel/TaskViewModel.kt
@@ -1,7 +1,38 @@
 package com.edsonjr.mytasks.ViewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.edsonjr.mytasks.Model.Task
 import com.edsonjr.mytasks.Repository.TaskRepository
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class TaskViewModel(private val repository: TaskRepository):ViewModel() {
+
+    val tasks = repository.tasks //lista de tasks vinda do repositorio (vindas do banco)
+
+
+    //metodo para inserir novas tasks no banco
+    fun insertTask(task: Task): Job = viewModelScope.launch {
+        repository.insert(task)
+    }
+
+
+    //metodo para atualizar uma task no banco
+    fun updateTask(task: Task): Job = viewModelScope.launch {
+        repository.update(task)
+    }
+
+
+    //metodo para remover uma task do banco
+    fun delete(task: Task): Job = viewModelScope.launch {
+        repository.delete(task)
+    }
+
+
+    //metodo para remover todos os dados do banco de dados
+    fun clearBD(): Job = viewModelScope.launch {
+        repository.deleteAll()
+    }
+
 }
