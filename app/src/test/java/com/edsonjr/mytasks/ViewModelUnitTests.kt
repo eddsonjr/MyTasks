dda64b4699d2590c407fcc307f500cad24c29a2c
eddsonjr <eddson.jr@gmail.com>
2021-08-14 18:49:14 -0400

    [Gradle][ViewModelUnitTest] - Adicionada as dependencias do testCore e do Roboletric no gradle e tambem criada a classe de ViewModelUnitTest para realizra os testes de viewmodel de tasks
    Signed-off-by: eddsonjr <eddson.jr@gmail.com>

---
+++ app/src/test/java/com/edsonjr/mytasks/ViewModelUnitTests.kt
@@ -0,0 +1,15 @@
+package com.edsonjr.mytasks
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+/*
+*   Classe para realizar testes no view Model
+*
+*/
+
+class ViewModelTests {
+
+}
