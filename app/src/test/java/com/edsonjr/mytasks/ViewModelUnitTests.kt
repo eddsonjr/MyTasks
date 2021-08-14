bbf7a69439746a86de89ad10897b5c7dfecf5607
eddsonjr <eddson.jr@gmail.com>
2021-08-14 18:53:50 -0400

    [ViewModelUnitTests] - Criadas as funcoes com os TODOs para os testes de CRUD usando o viewModel
    Signed-off-by: eddsonjr <eddson.jr@gmail.com>

--- app/src/test/java/com/edsonjr/mytasks/ViewModelUnitTests.kt
+++ app/src/test/java/com/edsonjr/mytasks/ViewModelUnitTests.kt
@@ -10,6 +10,36 @@ import org.robolectric.annotation.Config
 *
 */
 
+
+
 class ViewModelTests {
+
+    @Test
+    fun insertTask_unitTest() {
+        //TODO - CRIAR TESTE PARA INSERIR UMA TASK NO BANCO DE DADOS USANDO VM
+    }
+
+
+    @Test
+    fun deleteTask_unitTest() {
+        //TODO - CRIAR TESTE PARA REMOVER UMA TASK NO BANCO DE DADOS USANDO VM
+    }
+
+
+    @Test
+    fun updateTask_unitTest() {
+        //TODO - CRIAR UM TESTE PARA FAZER UPDATE DE UMA TASK NO BANCO USANDO VM
+    }
+
+
+    @Test
+    fun listTasks_unitTest(){
+        //TODO - CRIAR UM TESTE PARA LISTAR TODAS AS TASKS DO BANCO DE DADOS USANDO VM
+    }
+
+    @Test
+    fun deleteAllTasks_unitTest() {
+        //TODO - CRIAR UM TESTE PARA REMOVER TODAS AS TASKS DO BANCO DE DADOS USANDO VM
+    }
 
 }
