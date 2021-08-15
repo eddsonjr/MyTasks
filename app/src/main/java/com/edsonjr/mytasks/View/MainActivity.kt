da3720e6f8aded111eb995b1ba808f315b5ecfb3
eddsonjr <eddson.jr@gmail.com>
2021-08-15 19:23:17 -0400

    [MainActivity][activity_main.xml] - Criados os metodos para inicializar o viewmodel e tambem dar o start para o primeiro fragment
    Signed-off-by: eddsonjr <eddson.jr@gmail.com>

--- app/src/main/java/com/edsonjr/mytasks/View/MainActivity.kt
+++ app/src/main/java/com/edsonjr/mytasks/View/MainActivity.kt
@@ -2,11 +2,46 @@ package com.edsonjr.mytasks.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.edsonjr.mytasks.DataBase.TasksDatabase
 import com.edsonjr.mytasks.R
+import com.edsonjr.mytasks.Repository.TaskRepository
+import com.edsonjr.mytasks.View.Fragments.ListTasksFragment
+import com.edsonjr.mytasks.ViewModel.TaskViewModel
+import com.edsonjr.mytasks.ViewModel.TaskViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "[MainActivity]"
+    private lateinit var taskViewModel: TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViewModel()
+        startTaskListFragment()
     }
+
+
+    //metodo responsavel por inicializar o viewModel
+    private fun initViewModel(){
+        val dao = TasksDatabase.getDBInstance(application).TaskDAO //adquirindo o dao
+        val repository = TaskRepository(dao) //instanciando o repositorio que sera usado no factory
+        val factory = TaskViewModelFactory(repository)  //iniciando o viewmodel factory
+        this.taskViewModel = ViewModelProvider(this,factory).get(TaskViewModel::class.java)
+    }
+
+
+    //este metodo e responsavel por carregar o primeiro fragment, que mostra uma lista de
+    //tasks vindas do banco de dados
+    private fun startTaskListFragment() {
+        val listTaskFragment = ListTasksFragment()
+        val fragment = supportFragmentManager.beginTransaction()
+        fragment.replace(R.id.fragmentContainer,listTaskFragment)
+        fragment.commit()
+    }
+
+
 }
