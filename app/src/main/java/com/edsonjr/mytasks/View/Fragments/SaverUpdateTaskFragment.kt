1ca000911be21b82596b8df19b318b3d8ef7f360
eddsonjr <eddson.jr@gmail.com>
2021-08-17 18:37:10 -0400

    [SaverUpdateTaskFragment] - Criado um metodo para carregar os dados quando o usuario clica em alguma task no fragment anterior
    Signed-off-by: eddsonjr <eddson.jr@gmail.com>

--- app/src/main/java/com/edsonjr/mytasks/View/Fragments/SaverUpdateTaskFragment.kt
+++ app/src/main/java/com/edsonjr/mytasks/View/Fragments/SaverUpdateTaskFragment.kt
@@ -10,10 +10,13 @@ import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.ViewModelProvider
 import com.edsonjr.mytasks.DataBase.TasksDatabase
+import com.edsonjr.mytasks.Model.Task
 import com.edsonjr.mytasks.R
 import com.edsonjr.mytasks.Repository.TaskRepository
 import com.edsonjr.mytasks.ViewModel.TaskViewModel
 import com.edsonjr.mytasks.ViewModel.TaskViewModelFactory
+import com.edsonjr.mytasks.databinding.FragmentListTasksBinding
+import com.edsonjr.mytasks.databinding.FragmentSaverUpdateTaskBinding
 
 
 class SaverUpdateTaskFragment : Fragment() {
@@ -21,6 +24,8 @@ class SaverUpdateTaskFragment : Fragment() {
     private val viewModel: TaskViewModel by activityViewModels() //view model compartilhado
     private val TAG = "[SaveUpdateFrag]" //para fins de debug
 
+    private var _binding: FragmentSaverUpdateTaskBinding? = null
+    private val binding get() = _binding!!
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,17 +38,20 @@ class SaverUpdateTaskFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.fragment_saver_update_task, container, false)
+
+        //usando viewbinding para inflar e manipular o layout
+        _binding = FragmentSaverUpdateTaskBinding.inflate(inflater, container, false)
+        val view = binding.root
+
 
         //listener do fragment manager, responsavel por verificar se o usuario selecionou
         //uma task no fragment de listagem e carregar os dados na ui, permitindo update da task
         setFragmentResultListener("task_update"){ key, result ->
             val task = result.getSerializable("taskToUpdate")
             Log.d(TAG,"Task recebida: ${task.toString()}")
+            configUIToUpdateTask(task as Task)
         }
 
-
-        // Inflate the layout for this fragment
         return view
     }
 
@@ -56,8 +64,22 @@ class SaverUpdateTaskFragment : Fragment() {
 
 
     //metodo para carregar a view com dados vindos do fragment manager
+    private fun configUIToUpdateTask(task: Task){
+        //alterando os elementos visuais para comportar as informacoes das tasks
+        binding.btnCriarTarefa.setText(getString(R.string.btn_update_task)) //altera o texto do btn
+
+        binding.txtTaskTitle.editText?.setText(task.title)
 
 
+
+    }
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 
 
--- app/src/main/java/com/edsonjr/mytasks/View/MainActivity.kt
+++ app/src/main/java/com/edsonjr/mytasks/View/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         initViewModel()
         startTaskListFragment()
 
